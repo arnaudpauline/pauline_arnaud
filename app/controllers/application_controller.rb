8b3dfbd27ea9d7df5d52bf50240a13128e7faf14
arnaudpauline <root@LAPTOP-MH5N5B8D.localdomain>
2024-04-19 23:53:56 +0200

    last update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_beer_geek!
+  before_action :authenticate_beer_geek!
 
-    protected
+  protected
 
-    def authenticate_beer_geek!
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  def authenticate_beer_geek!
+    unless current_beer_geek
+      redirect_to new_beer_geek_session_path
     end
+  end
 end
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :beer_geeks
   devise_for :users
+  devise_for :beer_geeks, controllers: { sessions: 'beer_geeks/sessions', registrations: 'beer_geeks/registrations' }
   resources :beers
   resources :trucs
   resources :brands
-
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "beers#index"
